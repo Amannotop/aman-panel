1fced04bb1e9af6292cbabce74a7e93aedc7ede5
MacBook Air M4 <synzo@MacBooks-iMac-Pro.local>
2026-02-22 19:03:53 +0530

    Set baseURL for Render

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'https://aman-panel-1.onrender.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
